0385de73b33792eab7e1df27079b783251d111c8
adam <aabou9@gmail.com>
2020-06-19 21:20:33 +0100

    fix routes.rb typo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,17 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new
-
+        @user = User.new(user_params)
+        @user.save
     end
 
     def show
         @all_users = User.all
 
+    end
+    
+    def user_params
+
+        params.require(:user).permit(:username, :password)
     end
 end
