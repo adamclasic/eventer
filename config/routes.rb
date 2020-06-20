945c14fb5c4856d7f13196f5e18f6b52e39b7f2c
adam <aabou9@gmail.com>
2020-06-20 19:59:06 +0100

    add event to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :new, :show]
+  resources :events
   get "signin", to: 'sessions#new'
   post 'signin', to: 'sessions#create'
   delete 'signout', to: 'sessions#destroy'
