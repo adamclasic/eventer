811c1ec72c42662ff4c631b1bce8f61a7be7b7b4
adam <aabou9@gmail.com>
2020-06-20 21:48:13 +0100

    add helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    helper_method [:current_user, :logged_in?]
+  
+  
+    private 
+
+        def login(user)
+            session[:username] = user.username
+            session[:user_id] = user.id
+        end
+
+        def current_user
+            @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        end
+
+        def logged_in?
+            !current_user.nil?
+        end
+
+        def auth
+            redirect_to signin_path, alert: 'You must login to access that page' unless logged_in?
+        end
+    
 end
