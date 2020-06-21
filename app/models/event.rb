5f4d90ac28e9645f76fec27f74c6dab4ab24d3ed
adam <aabou9@gmail.com>
2020-06-21 16:48:44 +0100

    add validations to event.rb

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
+    validates_uniqueness_of :title, scope: :user_id
+    validates_length_of :title, :minimum => 3, :allow_nil => false
+    validates_length_of :description, :minimum => 3, :allow_nil => false
+    validates :date, presence: true
     belongs_to :creator, class_name: 'User', foreign_key: :user_id
 end
