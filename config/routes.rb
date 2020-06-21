c897f63186cb008ace9078c559dbf55e5ca317fc
adam <aabou9@gmail.com>
2020-06-21 09:42:15 +0100

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'events#index'
   resources :users, only: [:create, :new, :show]
   resources :events
   get "signin", to: 'sessions#new'
