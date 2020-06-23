4e11f630986c73ecee98366dda59cbaff38fb751
adam <aabou9@gmail.com>
2020-06-23 09:31:31 +0100

    add invitations table

---
+++ db/migrate/20200623082634_create_invitaion.rb
@@ -0,0 +1,6 @@
+class CreateInvitaion < ActiveRecord::Migration[5.2]
+  def change
+    create_table :invitaions do |t|
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_155459) do
+ActiveRecord::Schema.define(version: 2020_06_23_082634) do
 
   create_table "attendees", force: :cascade do |t|
     t.integer "event_id"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 2020_06_21_155459) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "invitaions", force: :cascade do |t|
   end
 
   create_table "users", force: :cascade do |t|
